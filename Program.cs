56a8b1d6293cc3b318dc0be101ee51ae08aea8ae
zalan <113088404+Zalan888@users.noreply.github.com>
2024-12-01 20:06:22 +0100

    Basics of mapDraw is added

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace battleShips
+{
+	internal class Program
+	{
+		static string[] xCords = {" A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "};
+
+		public static void mapDraw()
+		{
+			for (int i = 0; i < 11; i++)
+			{
+				for (int j = 0; j < 10; j++) 
+				{
+					if (i == 0)
+					{
+						Console.Write(xCords[j]);
+					}
+					else 
+					{
+                        Console.Write(" O ");
+					}
+                }
+                Console.WriteLine();
+                Console.WriteLine(" - - - - - - - - - - - - - - - ");
+			}
+		}
+		static void Main(string[] args)
+		{
+			mapDraw();
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("battleShips")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("battleShips")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29f15b38-37b6-4e27-8243-c0024a6d13db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
